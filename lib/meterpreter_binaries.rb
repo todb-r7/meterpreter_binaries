140615b32d413605881394880bf75e61971bcf45
OJ <oj@buffered.io>
2014-01-15 18:13:48 +1000

    Add metadata, reverse folder priority
    This commit makes MSF look in the local data/meterpreter folder before
    looking at the gem folder. This is to aid in development and testing,
    as copying files into the appropriate gem folder is painful. Instead,
    devs can copy directly to data/meterpreter and things will get picked
    up from there. If missing, the gem takes over.
    
    Details of the range of commits were added as metadata to the gemspec.
    The commit hash which represents the commit for meterpreter that was
    built into the gem is included, and the one for the previous release
    is also included. This is so that we can track the range of commits
    that the version contains.
    
    Previous commit hash is not included in the range, but the current
    one is.

--- lib/meterpreter_binaries.rb
+++ lib/meterpreter_binaries.rb
@@ -7,20 +7,28 @@ module MeterpreterBinaries
   EXTENSION_PREFIX      = 'ext_server_'
   METERPRETER_SUBFOLDER = 'meterpreter'
 
+  #
+  # Get the path to an extension based on its name (no prefix).
+  #
   def self.get_ext(ext_name, binary_suffix)
     return get("#{EXTENSION_PREFIX}#{ext_name}", binary_suffix)
   end
 
+  #
+  # Get the path to a meterpretr binary by full name.
+  #
   def self.get(name, binary_suffix)
     file_name = "#{name}.#{binary_suffix}".downcase
 
-    # try the local folder first to see if the extension exists
-    file_path = expand(local_dir, file_name)
+    # try the data folder first to see if the extension exists, as this
+    # allows for the MSF data/meterpreter folder to override what is
+    # in the gem. This is very helpful for testing/development without
+    # having to move the binaries to the gem folder each time.
+    file_path = expand(data_dir, file_name)
 
-    # if not, load it from the data folder as that's where the
-    # rest of the meterpreter stuff lives
+    # if not, load it from the local gem folder
     unless ::File.file?(file_path)
-      file_path = expand(data_dir, file_name)
+      file_path = expand(local_dir, file_name)
 
       # if it's still not valid, then return nil to indicate
       # that we can't find the extension
@@ -32,29 +40,49 @@ module MeterpreterBinaries
     return file_path
   end
 
+  #
+  # List all the available extensions for the given suffix.
+  #
   def self.list_extensions(binary_suffix)
-    # we shouldn't ever have the same files in two different
-    # locations, so it should be save to enumerate both and
-    # return a collection of the two combined
-    local = enum_ext(local_dir, binary_suffix)
-    data = enum_ext(data_dir, binary_suffix)
-    local + data
+    # Find the valid extensions in the data folder first.
+    extensions = enum_ext(data_dir, binary_suffix)
+
+    # merge in any that don't already exist in the gem folder
+    enum_ext(local_dir, binary_suffix).each do |e|
+      if !extensions.include?(e)
+        extensions.push(e)
+      end
+    end
+
+    return extensions
   end
 
 private
 
+  #
+  # Full path to the local gem folder which contains the binaries.
+  #
   def self.local_dir
     return ::File.join(::File.dirname(__FILE__), '..', METERPRETER_SUBFOLDER)
   end
 
+  #
+  # Full path to the MSF data folder which contains the binaries.
+  #
   def self.data_dir
     return ::File.join(Msf::Config.data_directory, METERPRETER_SUBFOLDER)
   end
 
+  #
+  # Expand the given root path and file name into a full file location.
+  #
   def self.expand(root_dir, file_name)
     return ::File.expand_path(::File.join(root_dir, file_name))
   end
 
+  #
+  # Enumerate extensions in the given root folder based on the suffix.
+  #
   def self.enum_ext(root_dir, binary_suffix)
     exts = []
     ::Dir.entries(root_dir).each do |f|
--- meterpreter_binaries.gemspec
+++ meterpreter_binaries.gemspec
@@ -10,8 +10,7 @@ Gem::Specification.new do |spec|
   spec.email         = ["info@rapid7.com"]
   spec.description   = %q{Compiled binaries for Metasploit's Meterpreter}
   spec.summary       = %q{This gem contains all the compiled binaries required to make
-                        Meterpreter function. This includes binaries for Windows, POSIX
-                        and Java.}
+                        Meterpreter function. This includes Windows and POSIX binaries.}
   spec.homepage      = "http://www.metasploit.com"
   spec.license       = '3-clause (or "modified") BSD'
 
@@ -19,6 +18,10 @@ Gem::Specification.new do |spec|
   spec.files        += Dir['meterpreter/*']
   spec.executables   = []
   spec.require_paths = ['lib']
+
+  spec.metadata                            = {}
+  spec.metadata['meterpreter_commit']      = '4b468c1403190f19db8b0d6d03f83a2b87247947'
+  spec.metadata['prev_meterpreter_commit'] = '69b15e87e9cc604071317aa1df21562306f2111c'
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
